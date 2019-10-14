392a377b12a97d602b63fe7babe2a2dd51676c0a
ferdia <ferdia@omko.co>
2019-10-14 22:05:11 +0100

    Added basic Student and Lesson models. Need to consider TimeSlot model.

--- class_bookings/admin.py
+++ class_bookings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Student, Lesson
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Lesson)
--- class_bookings/models.py
+++ class_bookings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    '''
+    Represents a student taking lessons on the site.
+    '''
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.email)
+
+
+class Lesson(models.Model):
+    '''
+    A polyglossa 1-on-1 lesson
+    '''
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    class_time = models.DateTimeField('The lesson time')
+    completed = models.BooleanField(default=False)
+    price = models.FloatField(default=20)
+
+    def __str__(self):
+        return "{} - {} - {} - Done?: {}".format(
+            self.student,
+            self.class_time,
+            self.price,
+            self.completed,
+            )
