f74c56ff9ee021611e751b35398dc7ebccbfb51c
ferdia <ferdia@omko.co>
2019-12-14 13:01:08 +0000

    Removed unused TemplateView import

--- class_bookings/urls.py
+++ class_bookings/urls.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-module-docstring
 from django.urls import path
-from django.views.generic import TemplateView
+
 from class_bookings import views
 import class_bookings.util as class_bookings_utils
 
