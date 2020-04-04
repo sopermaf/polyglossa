a04da558e5ebc1c789c9cb61e93b1587f4899053
ferdia <ferdia@omko.co>
2020-04-04 12:23:52 +0100

    Added seminar processor test

---
+++ payments/test/test_processors.py
@@ -0,0 +1,25 @@
+'''
+Tests for Payments/processors
+'''
+# pylint: disable=missing-function-docstring
+import json
+
+from .base_test import TestPayments
+
+from .. import processors
+
+class TestProcessors(TestPayments):
+    '''Tests for SeminarProcessor'''
+    def setUp(self):
+        super(TestProcessors, self).setUp()
+        self.serial_order = json.dumps(self.order_details)
+
+    # Tests
+
+    def test_seminar_complete(self):
+        processor = processors.SemSlotProcessor(self.serial_order)
+
+        self.assertFalse(self.student_in_seminar(), "Student absent")
+
+        processor.complete()
+        self.assertTrue(self.student_in_seminar(), "Student added")
