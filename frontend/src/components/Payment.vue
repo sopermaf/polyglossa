9ce68fd78cf073aca732fba5ca899abe85b08448
ferdia <ferdia@omko.co>
2020-04-12 11:32:14 +0100

    Loading paypal checkout button from assets folder

--- frontend/src/components/Payment.vue
+++ frontend/src/components/Payment.vue
@@ -3,18 +3,23 @@
         <v-layout justify-center wrap text-center class="mx-auto">
             <v-flex ma-5 lg6 s4 xs12>
                 <h1> Continue to Paypal </h1>
-                <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+                <v-form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
                     <input type="hidden" name="cmd" value="_s-xclick" />
                     <input type="hidden" name="encrypted" :value="button_address"/>
                     <input
                         type="image"
-                            src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png"
+                        :src="require('../assets/paypal-checkout-logo-large.png')"
                         
                         border="0"
                         name="submit"
                         alt="Check out with PayPal"
                     />
-                </form>
+                </v-form>
+            </v-flex>
+            <v-flex ma-5 lg6 s4 xs12>
+                <v-btn>
+                    hello world
+                </v-btn>
             </v-flex>
         </v-layout>
     </v-container>
@@ -34,7 +39,14 @@ export default {
         axios.get('/payments/button/').then(response => {
             console.log(response.data);
             this.button_address = response.data['button'];
+            console.log("Reached payments COmponent" + response.data);
         })
+    },
+    methods: {
+        cancelOrder() {
+            // cancel the order
+            // continue to form page
+        },
     },
 };
 </script>
