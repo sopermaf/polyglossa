e545aef550aedf0424c9abe3ba8a88f2e36f1cf9
ferdia <ferdia@omko.co>
2020-05-29 11:48:23 +0100

    Updated static folder to

--- polyglossa/settings.py
+++ polyglossa/settings.py
@@ -120,7 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(VUE_DIST_DIR, 'css'),
     os.path.join(VUE_DIST_DIR, 'img'),
