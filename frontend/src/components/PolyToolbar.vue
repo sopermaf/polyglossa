2487d9883b22357e38b1e57263651af8fdd63dbe
ferdia <ferdia@omko.co>
2020-06-06 10:55:07 +0100

    Polyglossa toolbar centred and added in expandable sidebar. links required

--- frontend/src/components/PolyToolbar.vue
+++ frontend/src/components/PolyToolbar.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-app-bar app class="banner">
+  <v-app-bar
+    app
+    class="banner"
+    hide-on-scroll
+  >
+   
+    <v-app-bar-nav-icon @click="openDrawer()"></v-app-bar-nav-icon>
 
-    <v-btn text @click="viewChoice('HOME')">
+    <!--<v-btn text @click="viewChoice('HOME')">
       <span class="mr-2">Home</span>
     </v-btn>
 
@@ -12,16 +18,17 @@
     <v-btn text @click="viewChoice('BOOKING')">
       <span class="mr-2">Book a Class</span>
     </v-btn>
-    
+    -->
     <v-spacer></v-spacer>
     
-    <v-toolbar-title class="headline text-uppercase" centre>
+    <v-toolbar-title class="headline text-uppercase">
       <span>Poly</span>
       <span class="font-weight-light">Glossa</span>
     </v-toolbar-title>
 
+    
     <v-spacer></v-spacer>
-
+    <!--
     <v-btn text  >
       <span class="mr-2">Exercises</span>
     </v-btn>
@@ -33,7 +40,7 @@
     <v-btn text  >
       <span class="mr-2">Contact Us</span>
     </v-btn>
-
+  -->
   </v-app-bar>
 </template>
 
@@ -46,6 +53,10 @@ export default {
   methods: {
     viewChoice(choice) {
       this.$emit("pageSelection", choice);
+    },
+    openDrawer() {
+      console.log("called");
+      this.$emit("drawer", true);
     }
   }
 }
