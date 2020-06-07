6021c445451f8f033c004ecc8d42f54a5da1088c
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2020-06-07 12:18:14 +0100

    Toolbar Navigation and Mobile Fit (#29)
    * Polyglossa toolbar centred and added in expandable sidebar. links required
    
    * Added mdi icons package
    
    * Added Page navigation and Social Media links in sidebar
    
    * Removed console.log from toolbar
    
    * NavBar visibility set for different screen sizes
    
    Co-authored-by: ferdia <ferdia@omko.co>

--- frontend/src/Index.vue
+++ frontend/src/Index.vue
@@ -1,10 +1,60 @@
 <template>
   <v-app>
     <!-- Header -->
-    <PolyToolbar @pageSelection="updateView" />
+    <PolyToolbar
+      @pageSelection="updateView"
+      @drawer="drawerVisible"
+      :isNavBarVisible="isNavBarVisible"
+    />
 
     <!-- Page Content -->
     <v-content>
+      
+      <template
+        v-if="isNavBarVisible">
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+        >
+          <v-list >
+            <!-- Navigation Items -->
+            <v-list-item
+              v-for="item in navItems"
+              :key="item.title"
+              @click="updateView(item.pageSelection)"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+          <!-- Social Media Items -->
+          <template
+            v-for="item in socialMediaItems"
+          >
+            <v-list-item
+              :key="item.title"
+              :href="item.pageLink"
+            >
+              <v-list-item-avatar>
+                <v-img :src="item.imageLink"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>{{item.title}}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-navigation-drawer>
+      </template>
+
       <template v-if="pageSelection == 'HOME'">
         <Home />
       </template>
@@ -36,6 +86,7 @@ import BookClassForm from "./components/BookClassForm";
 import PolyToolbar from "./components/PolyToolbar.vue";
 import PolyFooter from "./components/PolyFooter.vue";
 import Payment from "./components/Payment.vue"
+import {mdiClipboardTextOutline, mdiHome, mdiBookOpenPageVariant } from '@mdi/js'
 
 export default {
   name: "Index",
@@ -52,10 +103,38 @@ export default {
     courseChoice: null,
     order: null,
     button: null,
+    drawer: false,
+    isNavBarVisible: false,
+    navItems: [
+      {title: 'Home', icon: mdiHome, pageSelection: "HOME"},
+      {title: 'Courses', icon: mdiBookOpenPageVariant, pageSelection: "COURSES"},
+      {title: 'Book a class', icon: mdiClipboardTextOutline, pageSelection: "BOOKING"},
+    ],
+    socialMediaItems: [
+      {
+        title: 'Youtube',
+        imageLink: 'https://w7.pngwing.com/pngs/963/811/png-transparent-youtube-logo-youtube-red-logo-computer-icons-youtube-television-angle-rectangle.png',
+        pageLink: 'https://www.youtube.com/conneringles'
+      },
+      {
+        title: 'Instagram',
+        imageLink: 'https://p7.hiclipart.com/preview/477/609/118/logo-computer-icons-clip-art-instagram-logo.jpg',
+        pageLink: 'https://www.instagram.com/polyglossalanguages/'
+      },
+    ]
   }),
+  mounted() {
+    if(window.innerWidth < 800){
+      this.isNavBarVisible = true;
+    }
+  },
   methods: {
     updateView(view) {
       this.pageSelection = view;
+      this.drawer = false;
+    },
+    drawerVisible(val) {
+      this.drawer = val;
     },
     prefillForm(course) {
       this.pageSelection = "BOOKING";
@@ -70,3 +149,12 @@ export default {
   }
 };
 </script>
+
+
+<style >
+ @media screen and (max-width: 600px) {
+  body {
+    background-color: olive;
+  }
+}
+</style>
--- frontend/src/components/PolyToolbar.vue
+++ frontend/src/components/PolyToolbar.vue
@@ -1,51 +1,68 @@
 <template>
-  <v-app-bar app class="banner">
+  <v-app-bar
+    app
+    class="banner"
+    hide-on-scroll
+  >
 
-    <v-btn text @click="viewChoice('HOME')">
-      <span class="mr-2">Home</span>
-    </v-btn>
+    <template v-if="isNavBarVisible">
+      <v-app-bar-nav-icon @click="openDrawer()"></v-app-bar-nav-icon>
+    </template>
+    <template v-else>
+      <v-btn text @click="viewChoice('HOME')">
+        <span class="mr-2">Home</span>
+      </v-btn>
 
-    <v-btn text @click="viewChoice('COURSES')" >
-      <span class="mr-2">Courses</span>
-    </v-btn>
+      <v-btn text @click="viewChoice('COURSES')" >
+        <span class="mr-2">Courses</span>
+      </v-btn>
 
-    <v-btn text @click="viewChoice('BOOKING')">
-      <span class="mr-2">Book a Class</span>
-    </v-btn>
+      <v-btn text @click="viewChoice('BOOKING')">
+        <span class="mr-2">Book a Class</span>
+      </v-btn>
+    </template>
     
     <v-spacer></v-spacer>
     
-    <v-toolbar-title class="headline text-uppercase" centre>
+    <v-toolbar-title class="headline text-uppercase">
       <span>Poly</span>
       <span class="font-weight-light">Glossa</span>
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-btn text  >
-      <span class="mr-2">Exercises</span>
-    </v-btn>
+    <template v-if="!isNavBarVisible">    
+      <v-btn text  >
+        <span class="mr-2">Exercises</span>
+      </v-btn>
 
-    <v-btn text  >
-      <span class="mr-2">Texts</span>
-    </v-btn>
+      <v-btn text  >
+        <span class="mr-2">Texts</span>
+      </v-btn>
 
-    <v-btn text  >
-      <span class="mr-2">Contact Us</span>
-    </v-btn>
-
+      <v-btn text  >
+        <span class="mr-2">Contact Us</span>
+      </v-btn>
+    </template>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "PolyToolbar",
+  props: {
+    isNavBarVisible: {
+      type: Boolean
+    }
+  },
   data: () => ({
-
   }),
   methods: {
     viewChoice(choice) {
       this.$emit("pageSelection", choice);
+    },
+    openDrawer() {
+      this.$emit("drawer", true);
     }
   }
 }
