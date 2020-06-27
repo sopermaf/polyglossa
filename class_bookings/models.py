690eed7160709b2980aae43a49dd97008c98bf18
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2020-06-27 16:27:58 +0100

    Changed Student email to Email field from Char field (#39)
    - Email field in Student model updated 
    
    Co-authored-by: ferdia <ferdia@omko.co>

--- class_bookings/models.py
+++ class_bookings/models.py
@@ -20,7 +20,7 @@ class Student(models.Model):
     '''
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return f"{self.name}, {self.email}"
