70956c3b1130a6bef583d7f384079f4987a28cfa
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2020-07-12 11:07:48 +0100

    Update run_pipeline.sh
    Added echos to run_pipline.sh to show stage progress

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,8 +1,11 @@
 set -e
 
+echo "Running Django Tests"
 pipenv run python manage.py test
 
+echo "Running Python Linting"
 pipenv run pylint -d fixme payments polyglossa class_bookings
 
+echo "Running Frontend Linting"
 cd frontend/
 npm run lint
