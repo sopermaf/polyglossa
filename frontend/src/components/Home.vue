3f648a8041604dddd3e8036c18f7328e4d638a86
ferdia <ferdia@omko.co>
2020-07-16 19:14:19 +0100

    Main homepage text added

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,35 +2,67 @@
   <v-container>
     <v-layout text-center wrap justify-center>
 
-      <v-flex mt-5 mb-5 xs12>
-        <v-layout justify-center>
-          Flexible Online Language Classes and Resources
-        </v-layout>
-      </v-flex>
-
-      <v-flex mb-5 xs12>
+      <v-flex ma-5 lg12 xs12>
         <h3 class="display-1 font-weight-light mb-3">
-          <span>Learn English</span>
-          <span class="font-weight-bold">
-            Your
+          <span>Learn</span>
+          <span class="font-weight-bold font-italic">
+            Real
           </span>
-          <span>Way</span>
+          <span>English</span>
         </h3>
       </v-flex>
 
-      <v-flex mb-5 xs12>
-          <p v-for="i in 5" :key="i">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a nulla molestie, congue neque ut,
-            accumsan sapien. Nullam vestibulum est sed turpis consequat iaculis.
-            Sed semper vel tellus id varius. In ac lectus vel sem posuere porttitor sit amet quis lectus. Duis
-            tristique, nisl ut ullamcorper convallis, augue nisi elementum nibh, faucibus venenatis urna dui vitae dolor.
-            Quisque finibus sodales sem, vel pharetra enim. Ut sed magna maximus, feugiat massa eget, commodo ante.
-            Suspendisse potenti. Integer convallis mattis augue eu cursus. Sed et lacinia quam. Nunc vel neque 
-            justo. Sed blandit, justo sed tempor euismod, tortor erat euismod felis, ut blandit nisl urna non massa.
-             Mauris lorem risus, interdum vel lorem at, semper efficitur dolor. Maecenas eu semper ex. Ut pretium erat
-              nec aliquam fringilla.
+      <div class="break" />
+
+      <v-flex lg7 md8 sm10 xs12 >
+          <p>
+            Do you want to speak English
+            <span class="font-weight-bold">fluently</span>
+            and
+            <span class="font-weight-bold">confidently</span>
+            ?
+            Do you want to improve your listening and pronunciation?
+
+            Here at Polyglossa, our mission is to help you do exactly
+            that!
+            We want to help you speak English the way Americans really speak.
+            In our English seminars, you will learn how to:
           </p>
       </v-flex>
+      
+      <div class="break" />
+
+      <v-flex ma-2 lg2 md4 sm2 xs5 text-left>
+          <ul>
+            <li class="font-weight-bold">
+              increase your fluency
+            </li>
+            <li class="font-weight-bold">
+              improve your listening skills
+            </li>
+          </ul>
+      </v-flex>
+
+      <v-flex ma-2 lg3 md5 sm3 xs6 text-left>
+          <ul>
+            <li class="font-weight-bold">
+              speak with correct pronunciation
+            </li>
+            <li class="font-weight-bold">
+              use native expressions
+            </li>
+          </ul>
+      </v-flex>
+
+      <div class="break" />
+
+      <v-flex lg7 md8 sm10 xs12 ma-5>
+          <p>
+            If you want to speak English better and understand more, join one of our seminars today!
+          </p>
+      </v-flex>
+
+
     </v-layout>
   </v-container>
 </template>
