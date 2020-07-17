0efb7133f82cc057fe4db5c245debf10c4166063
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2020-07-17 10:47:02 +0100

    Contact Us Page (#43)
    * Added contact us to Index and side menu
    
    * Contact us button set to emit Contact us enum
    
    * Added ContactUs.vue page with basic contact details and socials
    
    * Social media hover and image contain added. Used detail instead of name
    
    * Social media updated, should be ported to backend db later
    
    * rebuilt frontend files
    
    * v-list used for social media items instead of v-card
    
    * Contact us info updated
    
    Co-authored-by: ferdia <ferdia@omko.co>

---
+++ frontend/src/components/ContactUs.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap justify-center>
+        <h1> Contact Us </h1>
+        
+        <div class="break" />
+
+        <v-flex ma-5 lg7 s12 xs12>
+            <p>
+                For any inquiries, questions, or problems,
+                please send an email to help@polyglossa.com.
+            </p>
+            <p>
+                Check out our social media channels for more English tips, lessons, and practice.
+            </p>
+        </v-flex>
+
+        <div class="break" />
+
+        <h1> Social Media </h1>
+
+        <div class="break" />
+
+        <v-flex ma-5 lg4 md4 s4 xs12 >
+            <v-list>
+                <v-list-item
+                    v-for="social in socialMedia" :key="social.title"
+                    :href="social.link"
+                    flat
+                >
+                    <v-list-item-avatar>
+                        <v-img :src="social.image" href="google.com" />
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ social.detail }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+      socialMedia: {
+          type: Array,
+          required: true,
+      }
+  },
+  data: () => ({
+    contactDetails: [
+      {name: 'Email', data: 'example@polyglossa.com'},
+    ],
+  }),
+};
+</script>
