43be339a7cfaf8a3ebd9ad222b13c66c1953e8ad
ferdia <ferdia@omko.co>
2020-08-28 19:04:03 +0100

    get upcoming seminars url added

--- class_bookings/urls.py
+++ class_bookings/urls.py
@@ -8,5 +8,6 @@ from . import const
 urlpatterns = [
     path('signup/seminar', views.post_seminar_student, name=const.SEMINAR_POST_NAME),
     path('get/seminar_slots/<int:seminar_id>', views.get_future_seminar_slots, name='sem-slots'),
-    path('get/activities/<str:activity_type>', views.get_activities, name='get-activities')
+    path('get/seminars/upcoming/', views.get_upcoming_seminars, name='get-upcoming-seminars'),
+    path('get/activities/<str:activity_type>', views.get_activities, name='get-activities'),
 ]
