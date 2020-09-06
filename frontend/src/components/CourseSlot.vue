493b62930e8f17bdf9823c1154d11b77f696e5e5
ferdia <ferdia@omko.co>
2020-09-06 12:13:41 +0100

    prototype seminar page for display to Conner made
    expand button made always visible. seminars now shown 2 side by side where possible

--- frontend/src/components/CourseSlot.vue
+++ frontend/src/components/CourseSlot.vue
@@ -6,18 +6,17 @@
             outlined
             :elevation="hover ? 16: 2" 
         >
-            <v-card-title class="justify-center mx-auto"> {{sem.title}} - ${{sem.price}} </v-card-title>
-            <v-card-text> {{sem.description}} </v-card-text>
+            <v-card-title class="justify-center mx-auto"> {{sem.title}} <br> ${{sem.price}} </v-card-title>
+            <v-card-text class="justify-left"> {{sem.description}} </v-card-text>
             <v-card-actions class="justify-center mx-auto" >
-            <v-expand-transition>
+            <!-- <v-expand-transition> -->
                 <v-btn
-                    v-if="hover"
                     @click="chooseCourse()"
                     class="d-flex transition-fast-in-fast-out v-card--reveal"
                 >
-                    Book Now
+                    Sign Up
                 </v-btn>
-            </v-expand-transition>
+            <!-- </v-expand-transition> -->
             </v-card-actions>
         </v-card>
     </v-hover>
