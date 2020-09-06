8977040d54dde91ea41f7de8dab93cf6c589f338
ferdia <ferdia@omko.co>
2020-09-06 22:23:01 +0100

    Title text wrapping fixed for seminars

--- frontend/src/components/CourseSlot.vue
+++ frontend/src/components/CourseSlot.vue
@@ -3,21 +3,33 @@
     <v-hover v-slot:default="{ hover }" open-delay>
 
         <v-card  
-            outlined
-            :elevation="hover ? 16: 2" 
+          outlined
+          :elevation="hover ? 16: 2" 
         >
-            <v-card-title class="justify-center mx-auto"> {{sem.title}} <br> ${{sem.price}} </v-card-title>
-            <v-card-text class="justify-left"> {{sem.description}} </v-card-text>
-            <v-card-actions class="justify-center mx-auto" >
-            <!-- <v-expand-transition> -->
-                <v-btn
-                    @click="chooseCourse()"
-                    class="d-flex transition-fast-in-fast-out v-card--reveal"
-                >
-                    Sign Up
-                </v-btn>
-            <!-- </v-expand-transition> -->
-            </v-card-actions>
+          <v-card-title
+            class="word-break-keep-all justify-center"
+          > 
+            {{sem.title}}
+            <br>
+            ${{sem.price}}
+          </v-card-title>
+          
+          <v-card-text
+            class="justify-left"
+          >
+            {{sem.description}}
+          </v-card-text>
+          
+          <v-card-actions
+            class="justify-center"
+          >
+            <v-btn
+              @click="chooseCourse()"
+              class="d-flex transition-fast-in-fast-out v-card--reveal"
+            >
+              Sign Up
+            </v-btn>
+          </v-card-actions>
         </v-card>
     </v-hover>
 
@@ -40,3 +52,9 @@ export default {
     }
 };
 </script>
+
+<style>
+.word-break-keep-all {
+  word-break: keep-all;
+}
+</style>
--- frontend/src/components/Courses.vue
+++ frontend/src/components/Courses.vue
@@ -6,7 +6,7 @@
         <h1> Seminars </h1>
       </v-flex>
 
-      <v-flex ma-2 lg4 md4 sm4 xs6 v-for="sem in mockSeminars" :key="sem">
+      <v-flex ma-2 lg4 md4 sm4 xs6 v-for="sem in seminars" :key="sem">
         <CourseSlot :sem="sem" @courseChoice="openForm"/>
       </v-flex>
 
@@ -29,14 +29,6 @@ export default {
   },
   data: () => ({
     courseChoice: null,
-    mockSeminars: [
-      {title: 'Basic English', price: '3.80', description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac lorem quis nisi porta egestas in id nisi. Suspendisse sit amet cursus leo. Sed blandit tincidunt nisl, at congue nibh vestibulum sit amet. Ut sagittis, ligula vulputate pulvinar convallis, metus purus ultricies urna, ut eleifend massa tellus mattis metus. Proin a est eget arcu vestibulum convallis a a risus. Vivamus ut massa nibh. Fusce convallis molestie libero pulvinar ultricies. Quisque vulputate eleifend libero vel ultrices. Nullam molestie quis nibh eget pellentesque. Aliquam faucibus euismod orci. Sed nec nunc felis. Suspendisse fermentum metus id ex sodales, ac pellentesque erat fermentum. In et metus at urna gravida semper gravida eget ex. Mauris quis blandit neque, in rutrum tellus. Mauris in mollis metus. Donec iaculis fermentum vulputate'},
-      {title: 'Grammar Essentials', price: '25', description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac lorem quis nisi porta egestas in id nisi. Suspendisse sit amet cursus leo. Sed blandit tincidunt nisl, at congue nibh vestibulum sit amet. Ut sagittis, ligula vulputate pulvinar convallis, metus purus ultricies urna, ut eleifend massa tellus mattis metus. Proin a est eget arcu vestibulum convallis a a risus. Vivamus ut massa nibh. Fusce convallis molestie libero pulvinar ultricies. Quisque vulputate eleifend libero vel ultrices. Nullam molestie quis nibh eget pellentesque. Aliquam faucibus euismod orci. Sed nec nunc felis. Suspendisse fermentum metus id ex sodales, ac pellentesque erat fermentum. In et metus at urna gravida semper gravida eget ex. Mauris quis blandit neque, in rutrum tellus. Mauris in mollis metus. Donec iaculis fermentum vulputate'},
-      {title: 'Word of the Day', price: '2.50', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac lorem quis nisi porta egestas in id nisi. Suspendisse sit amet cursus leo. Sed blandit tincidunt nisl, at congue nibh vestibulum sit amet. Ut sagittis, ligula vulputate pulvinar convallis, metus purus ultricies urna, ut eleifend massa tellus mattis metus. Proin a est eget arcu vestibulum convallis a a risus. Vivamus ut massa nibh. Fusce convallis molestie libero pulvinar ultricies. Quisque vulputate eleifend libero vel ultrices. Nullam molestie quis nibh eget pellentesque. Aliquam faucibus euismod orci. Sed nec nunc felis. Suspendisse fermentum metus id ex sodales, ac pellentesque erat fermentum. In et metus at urna gravida semper gravida eget ex. Mauris quis blandit neque, in rutrum tellus. Mauris in mollis metus. Donec iaculis fermentum vulputate'},
-      {title: 'Colloquial English', price: '2.99', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac lorem quis nisi porta egestas in id nisi. Suspendisse sit amet cursus leo. Sed blandit tincidunt nisl, at congue nibh vestibulum sit amet. Ut sagittis, ligula vulputate pulvinar convallis, metus purus ultricies urna, ut eleifend massa tellus mattis metus. Proin a est eget arcu vestibulum convallis a a risus. Vivamus ut massa nibh. Fusce convallis molestie libero pulvinar ultricies. Quisque vulputate eleifend libero vel ultrices. Nullam molestie quis nibh eget pellentesque. Aliquam faucibus euismod orci. Sed nec nunc felis. Suspendisse fermentum metus id ex sodales, ac pellentesque erat fermentum. In et metus at urna gravida semper gravida eget ex. Mauris quis blandit neque, in rutrum tellus. Mauris in mollis metus. Donec iaculis fermentum vulputate'},
-    ]
   }),
   methods: {
     openForm(seminar) {
