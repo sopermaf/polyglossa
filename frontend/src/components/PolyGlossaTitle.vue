93235b05c8fb66166a7598b22d1f163cb3040a1b
ferdia <ferdia@omko.co>
2020-09-06 16:06:24 +0100

    added css version of logo with correct colors
    much lower in size required

---
+++ frontend/src/components/PolyGlossaTitle.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1> 
+    <span class="yellow--text text--lighten-2">Poly</span>
+    <span class="orange--text text--accent-2">glossa</span>
+  </h1>
+</template>
--- frontend/src/components/PolyToolbar.vue
+++ frontend/src/components/PolyToolbar.vue
@@ -3,54 +3,70 @@
     app
     class="banner"
     color="#2f3253"
+    prominent
   >
 
-    <span
-      class="d-flex d-sm-none"
-    >
-      <v-app-bar-nav-icon @click="openDrawer()"></v-app-bar-nav-icon>
-    </span>
-    
-    <span
-      class="hidden-xs-only"
-      v-for="b in preLogoButtons" :key="b.export">
-      <v-btn
-        text
-        @click="viewChoice(b.export)"
-        class="white--text"
-      >
-        <span class="mr-2">{{b.text}}</span>
-      </v-btn>
-    </span>
-    
-    <v-spacer></v-spacer>
-    
-    <v-img
-      :src="require('../assets/logo.png')"
-      contain
-      height="100%"
-    ></v-img>
+    <v-container>
+      <v-layout row justify-center class="">
+        <span
+          class="d-flex-box-inline h1 mt-1"
+        >
+          <PolyGlossaTitle />
+        </span>
+      </v-layout>
 
-    <v-spacer></v-spacer>
-
-    <span
-      class="hidden-xs-only"
-      v-for="b in postLogoButtons" :key="b.export"
-    >
-      <v-btn
-        text
-        @click="viewChoice(b.export)"
-        class="white--text"
+      <v-layout row justify-center class="mb-4">
+      <span
+        class="d-flex-box-inline d-md-none"
       >
-        <span class="mr-2">{{b.text}}</span>
-      </v-btn>
-    </span>
+        <v-app-bar-nav-icon
+          @click="openDrawer()"
+        >
+        </v-app-bar-nav-icon>
+      </span>
+      
+      <span
+        class="d-none d-md-flex"
+        v-for="b in preLogoButtons" :key="b.export"
+      >
+        <v-btn
+          text
+          @click="viewChoice(b.export)"
+          class="white--text"
+        >
+          <span class="mr-2">{{b.text}}</span>
+        </v-btn>
+      </span>
+
+      <v-spacer></v-spacer>
+      
+      <v-spacer class=""></v-spacer>
+
+      <span
+        class="d-none d-md-flex"
+        v-for="b in postLogoButtons" :key="b.export"
+      >
+        <v-btn
+          text
+          @click="viewChoice(b.export)"
+          class="white--text"
+        >
+          <span class="mr-2">{{b.text}}</span>
+        </v-btn>
+      </span>
+      </v-layout>
+    </v-container>
   </v-app-bar>
 </template>
 
 <script>
+import PolyGlossaTitle from "./PolyGlossaTitle";
+
 export default {
   name: "PolyToolbar",
+   components: {
+    PolyGlossaTitle,
+  },
   data: () => ({
     preLogoButtons: [
       {text: 'Home', export: 'HOME'},
