fc02f9a3533afef91fb2f976813dab295c7ae3c8
ferdia <ferdia@omko.co>
2020-12-04 20:44:43 -0600

    django-admin custom task for sending unsent emails added

---
+++ tasks/management/commands/send_emails.py
@@ -0,0 +1,15 @@
+# pylint: disable=missing-class-docstring,import-error
+from django.core.management.base import BaseCommand, CommandError
+from tasks.models import EmailTask
+
+
+class Command(BaseCommand):
+    help = 'Send all unsent email tasks'
+
+    def handle(self, *args, **options):
+        for email_task in EmailTask.unsent.all():
+            email_task.send()
+            self.stdout.write(
+                self.style.SUCCESS('EmailTask(id=%r) sent' % email_task.id)
+            )
+        self.stdout.write(self.style.SUCCESS('All emails sent'))
