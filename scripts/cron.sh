f5c2e4396409649dea4a2ba6c256953f0b1f18ca
ferdia <ferdia@omko.co>
2020-12-10 23:24:17 -0600

    cron job updated with send_seminar_emails

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -2,8 +2,11 @@
 PYTHON="/home/sopermaf/.virtualenvs/polyglossa-env/bin/python"
 MANAGE_PY="/home/sopermaf/polyglossa/manage.py"
 
-echo "Starting sending emails"
+echo "Starting sending emails\n\n"
 $PYTHON $MANAGE_PY send_emails_tasks
+
+echo "Starting seminar reminder emails\n\n"
+$PYTHON $MANAGE_PY send_seminar_reminders
 
 echo "Sleeping for 5 mins"
 sleep 5m
