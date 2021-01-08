30a0a623a18b348781af5d2290e31cee53ef6b00
ferdia <ferdia@omko.co>
2021-01-07 18:53:24 -0600

    fixed references to python in cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,10 +4,10 @@ source /home/sopermaf/.virtualenvs/polyglossa-env/bin/activate
 MANAGE_PY="/home/sopermaf/polyglossa/manage.py"
 
 echo "Starting sending emails\n\n"
-$PYTHON $MANAGE_PY send_email_tasks
+python $MANAGE_PY send_email_tasks
 
 echo "Starting seminar reminder emails\n\n"
-$PYTHON $MANAGE_PY send_seminar_reminders
+python $MANAGE_PY send_seminar_reminders
 
 echo "Sleeping for 5 mins"
 sleep 5m
