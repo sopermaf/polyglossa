8d9c997fb8313d4653aa42b1c25b2d8a8c885b80
ferdia <ferdia@omko.co>
2021-01-07 18:45:46 -0600

    db backup updated to retain only the 3 most recent days of backups

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# no fail on error flag in case of failure to remove files 
 
 timestamp=`date +%Y-%m-%d`
 
@@ -7,4 +7,8 @@ timestamp=`date +%Y-%m-%d`
 db_backup_file="db-backup_$timestamp.sql"
 
 cd
+
+# remove all but the 3 most recent backups
+ls -tp db-backup_*.txt | tail -n +4 | tr '\n' '\0' | xargs -0 rm --
+
 mysqldump -u sopermaf -h sopermaf.mysql.pythonanywhere-services.com --set-gtid-purged=OFF 'sopermaf$polyglossa'  > $db_backup_file
