b179345f44f53db9b5ba88c7a27b08f16403b40b
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2021-09-04 15:48:35 +0100

    basic feedback admin setup

--- feedback/admin.py
+++ feedback/admin.py
@@ -4,5 +4,21 @@ from django.contrib import admin
 from . import models
 
 
-admin.register(models.FeedbackType)
-admin.register(models.Feedback)
+@admin.register(models.FeedbackType)
+class FeedbackTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(models.Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        'feedback_type',
+        'status',
+        'reference',
+        'modified',
+        'created',
+    )
+    ordering = ('-modified', '-created')
+    search_fields = ('reference', 'detail')
+    list_filter = ('feedback_type', 'status')
+    readonly_fields = ('created', 'modified')
